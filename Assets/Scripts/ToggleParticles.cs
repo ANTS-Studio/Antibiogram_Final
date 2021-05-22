dd4e0e1c0eaca286dc36b736be0b00b3f004af29
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2021-05-22 16:39:23 +0200

    Genericka skripta za pokretanje particles

--- Assets/Scripts/ToggleParticles.cs
+++ Assets/Scripts/ToggleParticles.cs
@@ -7,11 +7,8 @@ public class ToggleParticles : MonoBehaviour
     public Camera mainCam;
     public LayerMask layerMask;
     public Text interactText;
-    public ParticleSystem particles;
 
     public float maxDistance = 70f;
-
-    private bool activeParticles = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,23 +20,28 @@ public class ToggleParticles : MonoBehaviour
     void Update()
     {
         Ray ray = mainCam.ViewportPointToRay(Vector3.one / 2f);
-        Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red); 
+        //Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red); 
 
         RaycastHit hitInfo;
 
+        // Trazi item koji se nalaze na odredenom layeru
         if (Physics.Raycast(ray, out hitInfo, maxDistance, layerMask))
         {
-            interactText.text = "Press 'E' to interact";
+            // Dohvati particle system pogodenog itema, ako ne postoji particleSystem return
+            ParticleSystem particles = hitInfo.collider.gameObject.GetComponentInChildren<ParticleSystem>();
+            if (!particles) return;
 
-            if (Input.GetKeyDown(KeyCode.E) && !activeParticles)
+            // Prikazi poruku playeru
+            interactText.text = "Press 'E' to turn interact";
+
+            // Na pritisak 'E' pokreni/stopiraj dohvaceni particle system
+            if (Input.GetKeyDown(KeyCode.E) && !particles.isPlaying)
             {
                 particles.Play();
-                this.activeParticles = true;
             }
-            else if (Input.GetKeyDown(KeyCode.E) && activeParticles)
+            else if (Input.GetKeyDown(KeyCode.E) && particles.isPlaying)
             {
                 particles.Stop();
-                this.activeParticles = false;
             }
         }
         else
